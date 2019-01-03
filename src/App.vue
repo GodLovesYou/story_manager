7ff98b2dd4456bc3af93b6ca4460d1a3c1f99cac
GodLovesYou <1593370493@qq.com>
2019-01-03 20:00:04 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  @import "../static/css/main.css";
+  @import "../static/css/color-dark.css";
+</style>
---
+++ src/components/common/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <v-head></v-head>
+    <v-side-bar></v-side-bar>
+    <div class="content">
+      <transition name="move" mode="out-in"><router-view></router-view></transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import vHead from './Header'
+import vSideBar from './SideBar'
+export default {
+  name: 'Home',
+  components: {
+    vHead, vSideBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
